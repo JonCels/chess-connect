3aad82963bbe6c72c804a47347056286f62bd9e5
vankraa <alexvankralingen@gmail.com>
2023-01-18 02:31:31 -0500

    added various Chessboard functions to src. Began MIS documentation for chess board and arduino

---
+++ src/ChessBoard_2.cpp
@@ -0,0 +1,46 @@
+#include <string>
+enum COLOUR{
+    white,
+    black        
+};
+
+struct Piece
+{
+    std::string Name;
+    COLOUR Colour;
+    std::string FEN;
+};
+
+struct Position
+{
+    int X;
+    int Y;
+};
+
+
+class ChessBoard
+{
+private:
+    /* data */
+    void movePiece(Position start, Position end);
+    bool isValidMove(Piece piece);
+    bool removePiece(Piece piece, Position position);
+    bool isCheck();
+    bool isCheckmate();
+    std::string translateToFEN();
+    
+
+public:
+    ChessBoard(/* args */);
+    ~ChessBoard();
+    
+};
+
+ChessBoard::ChessBoard(/* args */)
+{
+}
+
+ChessBoard::~ChessBoard()
+{
+}
+
