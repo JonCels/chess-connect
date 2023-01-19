ed717451514ecf6ad403aa7f16190d86eddeb05b
Joshua.Chapman <chapmj3@mcmaster.ca>
2023-01-18 23:58:19 -0500

    Final changes

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <piece-identification.cpp>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String id = ReadSensors(analogRead(0), analogRead(1))
+}
