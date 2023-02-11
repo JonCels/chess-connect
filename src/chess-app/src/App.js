641911a31d4e5f47660e5396b9bcde2cd95a5116
Rupinder Nagra <rupinder0410@gmail.com>
2023-02-11 11:17:27 -0500

    added best move calculation

--- src/chess-app/src/App.js
+++ src/chess-app/src/App.js
@@ -5,41 +5,39 @@ import { Chess } from 'chess.js'
 import { calculateBestMove, initGame } from "chess-ai";
 
 const container = {
-  marginTop: "2rem",
+  marginTop: "6rem",
   display: "flex",
   justifyContent: "space-around",
   alignItems: "center"
 }
 
-function lichessCloudEval(fen) {
-  return new Promise(function (resolve, reject) {
-    fetch("https://lichess.org/api/cloud-eval?fen=" + fen + "&multiPv=" + "3")
-      .then((response) => response.json())
-      .then((data) => resolve(data["pvs"]));
-  }
-)}
+// function lichessCloudEval(fen) {
+//   return new Promise(function (resolve, reject) {
+//     fetch("https://lichess.org/api/cloud-eval?fen=" + fen + "&multiPv=" + "3")
+//       .then((response) => response.json())
+//       .then((data) => resolve(data["pvs"]));
+//   }
+// )}
 
-async function getTopThree(fenString) {
-  const topThree = []
-  const moveVariations = await lichessCloudEval(fenString);
-  for(let i = 0; i < moveVariations.length; i++) {
-    topThree.push(moveVariations[i]["moves"].split(" ").shift())
-  }
-  return topThree
-}
+// async function getTopThree(fenString) {
+//   const topThree = []
+//   const moveVariations = await lichessCloudEval(fenString);
+//   for(let i = 0; i < moveVariations.length; i++) {
+//     topThree.push(moveVariations[i]["moves"].split(" ").shift())
+//   }
+//   return topThree
+// }
 
 function App() {
 
   const fenString = "rnbqkbnr/pp2pppp/2p5/3p4/3PP3/5P2/PPP3PP/RNBQKBNR b KQkq - 0 3"
-  // const chess = new Chess(
-  //   'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-  // )  
-  // const difficulty = 1
-  // initGame(chess, difficulty)
-  // const bestMove = calculateBestMove(chess, difficulty);
-  // console.log(bestMove)
+  const chess = new Chess(fenString)  
+  const difficulty = 1
+  initGame(chess, difficulty)
+  const bestMove = calculateBestMove(chess, difficulty);
+  console.log(bestMove)
 
-  console.log(getTopThree(fenString))
+  // console.log(getTopThree(fenString))
   const [fen, setFen] = useState(fenString)
 
   return (
